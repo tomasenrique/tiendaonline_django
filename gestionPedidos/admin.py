5125e2691ef4ba1800a151dcbf51a8a399f5295d
Tomas Estrada <tomasenriquea@hotmail.com>
2020-07-29 05:13:29 +0200

    Panel de control, Se crea una clase que se encargara de:  configurar el apartado de clientes para que se muestre los datos como si fuera un tabla especificando los campos que se quieran ver, se agrega una barra de busqueda con los campos especificados. Pruebas Ok

--- gestionPedidos/admin.py
+++ gestionPedidos/admin.py
@@ -7,11 +7,18 @@ from gestionPedidos.models import Clientes, Articulos, Pedidos  # importando las
 Aqui es donde se codificara todo lo necesario para poder gestionar las tablas en la BBDD
 
 OJO: Puede que para algunas modificaciones haya que reiniciar el servidor para poder aplicar los cambios.
-
 """
 
-admin.site.register(Clientes)
+""" 
+Con esta clase se configura que campos mostrar en la vista como si fuera una tabla, se aplicara luego en la linea 
+correspondiente """
+
+
+class clientesAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "direccion", "telefono")  # Para mostrar los campos que querramos en la vista
+    search_fields = ("nombre", "telefono")  # Para agregar una barra de busqueda con los campos especificados
+
+
+admin.site.register(Clientes, clientesAdmin)
 admin.site.register(Articulos)
 admin.site.register(Pedidos)
-
-
--- gestionPedidos/models.py
+++ gestionPedidos/models.py
@@ -6,13 +6,22 @@ from django.db import models
 #  Aqui se estan creando las clases que luego pasaran a ser las tablas
 class Clientes(models.Model):
     nombre = models.CharField(max_length=30)
-    direccion = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=50, verbose_name="La dirección")
     email = models.EmailField(blank=True, null=True)
     telefono = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.nombre
+
 
 """
- Al agregar 'blank=True, null=True' se hace que el campo sea opcional a la hora de crear un cliente en la web
+ Al agregar 'blank=True, null=True' se hace que el campo sea opcional a la hora de crear un cliente en la web, requiere reiniciar servidor
+ 
+ Al usar 'verbose_name=' se esta cambiando el nombre con el que se muestra en la vista de la pagina web, no necesita reiniciar servidor
+ 
+ 
+ 
+ 
 """
 
 
